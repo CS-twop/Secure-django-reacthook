8fdf7619320f8dd7f88664298d7d9c2e944cd1ca
winpasit <42579006+winpasit@users.noreply.github.com>
2020-11-12 00:08:17 +0700

    post init

---
+++ frontend/src/Pages/Post.js
@@ -0,0 +1,25 @@
+import './Post.css';
+
+function Post() {
+    return (
+        <div className='post'>
+            <div className='post-header'>
+                <div className='poster-detail'>
+                    Neo.
+                </div>
+                <div className='post-btns'>
+                    <input type='button' value='edit'></input>
+                    <input type='button' value='delete'></input>
+                </div>
+            </div>
+            <div className='post-context'>
+                vvdfvddfvdvdvdvdvddedfdffddfHHHdvdvdvdvvdv
+            </div>
+            <div className='comment-part'>
+                fdfdfdf
+            </div>
+        </div>
+    )
+}
+
+export default Post;
--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Homepage from './Pages/Homepage';
 import Signup from './Pages/Signup';
 import Signin from './Pages/Signin';
+import Post from './Pages/Post';
 
 function Routes() {
     return (
@@ -11,6 +12,7 @@ function Routes() {
                 <Route path='/' exact component={Homepage} />
                 <Route path='/signup' component={Signup} />
                 <Route path='/signin' component={Signin} />
+                <Route path='/post' component={Post} />
                 <Route render={() => <h1>404: page not found</h1>} />
             </Switch>
         </Router>
