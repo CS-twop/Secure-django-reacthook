eb17b488f7669853333750b207763ccc6d18059b
winpasit <42579006+winpasit@users.noreply.github.com>
2020-11-11 20:22:13 +0700

    signup page init

--- frontend/src/Pages/Signup.js
+++ frontend/src/Pages/Signup.js
@@ -1,22 +1,35 @@
+import './Signup.css';
 
 function Signup () {
     return (
-        <form>
-            <h1>Signup Page</h1>
-            <label>Username:</label>
-            <input
-                type='text'
-                name='username'
-                placeholder='username'
-            />
-            <label>Password:</label>
-            <input
-                type='password'
-                name='password'
-                placeholder='password'
-            />
-            <input type='submit' />
-        </form>
+        <div className='page'>
+            <div className='signup-box'>
+                <form>
+                    <div className='sigup-header'>web/signup.</div>
+                    <div className='signup-subbox' id='usrn'>
+                        <label className='label'>Username:</label>
+                        <input
+                            className='input-authen'
+                            type='text'
+                            name='username'
+                            placeholder='Put your nice name'
+                        />
+                    </div>
+                    <div className='signup-subbox' id='pwd'>
+                        <label className='label'>Password:</label>
+                        <input
+                            className='input-authen'
+                            type='password'
+                            name='password'
+                            placeholder='Put your secure password'
+                        />
+                    </div>
+                    <div className='signup-btn-subbox'>
+                        <input className='signup-btn' type='submit' value='sign up' />
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
 
