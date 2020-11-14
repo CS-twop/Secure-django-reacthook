a0e21a9df29ebe72ff32f2571b9764a0f2d50f11
winpasit <42579006+winpasit@users.noreply.github.com>
2020-11-14 23:13:22 +0700

    remove homepage and signup from route

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,17 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Homepage from './Pages/Homepage';
-import Signup from './Pages/Signup';
+// import Homepage from './Pages/Homepage';
+// import Signup from './Pages/Signup';
 import Signin from './Pages/Signin';
 import Forum from './Pages/Forum';
 
 function Routes() {
     return (
         <Router>
-            <Switch>
-                <Route path='/' exact component={Homepage} />
-                <Route path='/signup' component={Signup} />
-                <Route path='/signin' component={Signin} />
+            <Switch> 
+                <Route path='/signin' exact component={Signin} />
                 <Route path='/forum' component={Forum} />
                 <Route render={() => <h1>404: page not found</h1>} />
             </Switch>
