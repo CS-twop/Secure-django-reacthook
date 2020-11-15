f083fea929545240902f60a4deb2f527adf9f591
owenthanapat229@gmail.com <owenthanapat229@gmail.com>
2020-11-15 21:10:07 +0700

    add axios and handle signin

--- frontend/src/Pages/Signin.js
+++ frontend/src/Pages/Signin.js
@@ -1,10 +1,36 @@
 import './Signin.css';
+import React,{useState} from "react";
+import axiosInstance from "../axiosApi";
+
 
 function Signin () {
+    const [username,setUsername] = useState("")
+    const [password,setPassword] = useState("")
+
+    const handleSubmit = () => {
+        // console.log(username)
+        // console.log(password)
+        let json = {
+            "username": username,
+            "password": password
+        }
+        console.log(json)
+        try{
+            axiosInstance.post(`token/obtain`,json)
+            .then((response) => {
+                axiosInstance.defaults.headers['Authorization'] = "JWT " + response.data.access;
+                localStorage.setItem('access_token', response.data.access);
+                localStorage.setItem('refresh_token', response.data.refresh);
+                alert(response.data)
+            })
+        } catch(error){
+            throw error
+        }
+    }
     return (
         <div className='page'>
             <div className='signin-box'>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className='signin-header'>web/signin.</div>
                     <div className='signin-subbox' id='usrn'>
                         <label className='label'>Username:</label>
@@ -13,7 +39,10 @@ function Signin () {
                             className='input-authen'
                             type='text'
                             name='username'
+                            value = {username}
+                            onChange = {(e)=> setUsername(e.target.value)}
                             placeholder='Put your nice username'
+                            required
                         />
                     </div>
                     <div className='signin-subbox' id='pwd'>
@@ -22,11 +51,14 @@ function Signin () {
                             className='input-authen'
                             type='password'
                             name='password'
+                            value = {password}
+                            onChange = {(e)=> setPassword(e.target.value)}
                             placeholder='Put your secure password'
+                            required
                         />
                     </div>
                     <div className='signin-btn-subbox'>
-                        <input className='signin-btn' type='submit' value='sign in' />
+                        <input className='signin-btn' type='submit'  value='sign in' />
                     </div>
                 </form>
             </div>
