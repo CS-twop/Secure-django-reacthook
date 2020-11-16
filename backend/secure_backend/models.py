f6c4fce670eb18bd06e7efda60d6a2f02829c224
Peeranuth Kehasukcharoen <peeranuth.k@gmail.com>
2020-11-16 12:33:39 +0700

    Add: create/update time of post&comment

--- backend/secure_backend/models.py
+++ backend/secure_backend/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     user = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
     content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     # created_time = models.DateTimeField('created time')
 
     def __str__(self):
@@ -15,6 +17,8 @@ class Comment(models.Model):
     post_id = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
     content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.user) + ' >> ' + self.content
